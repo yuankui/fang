20a01273eeb9180f574489509a34ed64d0174bb6
yuankui <youzan@yuankui.com>
2016-10-07 19:30:14 +0800

    update house admin

--- house/admin.py
+++ house/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from house.models import OldHouse
+
+admin.site.register(OldHouse)
--- house/models.py
+++ house/models.py
@@ -4,5 +4,8 @@ from django.db import models
 # Create your models here.
 
 class OldHouse(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=100)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
